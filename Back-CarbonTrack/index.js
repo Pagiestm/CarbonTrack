a639ebd7ec164771b7d0f40bbae54e5f2f0a1421
pagiestm <pagiestm@gmail.com>
2024-07-25 18:44:50 +0200

    fix change config

--- Back-CarbonTrack/index.js
+++ Back-CarbonTrack/index.js
@@ -6,15 +6,8 @@ import cors from 'cors';
 
 const app = express();
 const port = 3000;
-const host = '0.0.0.0'; // Listen on all network interfaces
 
-// Configure CORS to allow requests from your front-end domain
-const corsOptions = {
-  origin: 'https://carbontrack.mds-lille.yt',
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
@@ -22,6 +15,6 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth', authRouter);
 
-app.listen(port, host, () => {
-    console.log(`App started, listening on https://${host}:${port}`);
+app.listen(port, () => {
+    console.log(`App started, listening to port ${port}`);
 });
