f44fa3a1b9a8d8893f781012e1c37b69fb7dc544
pagiestm <pagiestm@gmail.com>
2024-07-25 21:04:40 +0200

    fix(index.js): add cors

--- Back-CarbonTrack/index.js
+++ Back-CarbonTrack/index.js
@@ -7,11 +7,24 @@ import cors from 'cors';
 const app = express();
 const port = 3000;
 
+// Autoriser toutes les origines
 app.use(cors());
 
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
 
+// Middleware pour gérer les requêtes OPTIONS
+app.options('*', cors());
+
+// Ajout manuel des en-têtes CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+// Routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth', authRouter);
 
