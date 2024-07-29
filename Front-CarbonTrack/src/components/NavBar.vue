6d08bceca97b4c05ed14736377f4a2040d26cc97
pagiestm <pagiestm@gmail.com>
2024-07-29 21:47:07 +0200

    feat(shema.prisma): Adding the role field to the User table

--- Front-CarbonTrack/src/components/NavBar.vue
+++ Front-CarbonTrack/src/components/NavBar.vue
@@ -1,10 +1,8 @@
 <template>
     <nav class="bg-gray-800 p-4">
         <div class="container mx-auto flex justify-between items-center">
-            <div class="text-white text-lg font-semibold">MyApp</div>
+            <router-link to="/" class="text-white text-lg font-semibold">MyApp</router-link>
             <div>
-                <router-link to="/"
-                    class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</router-link>
                 <router-link v-if="!isAuthenticated" to="/login"
                     class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</router-link>
                 <router-link v-if="isAuthenticated" to="/profile"
