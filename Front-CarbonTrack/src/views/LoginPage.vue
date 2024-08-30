4a08929281b9040db93c83fc36f8d96fa67885a7
pagiestm <pagiestm@gmail.com>
2024-08-30 17:21:20 +0200

    style(LoginPage.vue): google login button in white + addition of the icon

--- Front-CarbonTrack/src/views/LoginPage.vue
+++ Front-CarbonTrack/src/views/LoginPage.vue
@@ -26,7 +26,8 @@
                     </button>
                 </form>
                 <button @click="loginWithGoogle"
-                    class="w-full mt-4 px-4 py-2 bg-blue-500 text-white font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    class="w-full mt-4 px-4 py-2 bg-white text-gray-700 font-medium rounded-md shadow-sm border border-gray-300 flex items-center justify-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png" alt="Google Logo" class="w-5 h-5 mr-2">
                     Connexion avec Google
                 </button>
                 <div class="mt-6 text-center">
