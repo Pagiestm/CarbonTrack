f16317c402bae7059e2ad216975129c7c13eeb88
pagiestm <pagiestm@gmail.com>
2024-08-30 15:20:09 +0200

    feat(env): add .env.local file

--- Back-CarbonTrack/Services/auth.services.js
+++ Back-CarbonTrack/Services/auth.services.js
@@ -8,6 +8,9 @@ import fs from 'fs';
 import path from 'path';
 import handlebars from 'handlebars';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
 
 const prisma = new PrismaClient();
 
